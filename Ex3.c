3a248de125f15f6e091667c2a2bb4af7c1f12f9c
elonezra <elonezra1995@hotmail.com>
2019-12-16 21:40:46 +0200

    elon-v1

--- Ex3.c
+++ Ex3.c
@@ -1,22 +1,63 @@
 #include <stdio.h>
-# define 256 LINE 
+# define LINE 256
+# define WORD 30
 
-int getline(char);
+int similar(char *s, char *t,int n);
 
-int main(){
-
+int main()
+{
+  char s[] = "swsystems";
+  char t[] = "ssysems";
+  int result = similar(s, t,2) ;
+  printf(" %d",result);
 }
 
-int getline(char s[])
+int getword(char w[])
 {
-    int length=0;
-    char c = ' ';
-    s= {0};
-    while ((c= getchar()!='\0') && length<=line)
-    {
-        length++;
-      s+=c;
-    }
-   
+  int count = 0;
+  for(int i = 0; w[i] != '\0' || w[i] != '\n'&& w[i] != '\t' && i<30;i++)
+  {
+    count++;
+  }
+    return count;
+}
 
+
+int similar(char *s, char *t,int n)
+{
+/// s can be modifaied
+ int _n = n;
+ int s_index = 0, t_index = 0;
+
+  while ( *(s+s_index) != '\0' || *(t+t_index) != '\0')
+  {
+
+      if( *(s+s_index) != *(t+t_index) )
+      {
+
+        if (n >= 0)
+        {
+          _n--;
+         t_index--;
+          
+        }
+        else
+        {
+          break;
+        }
+        
+
+      }
+      t_index++;
+      s_index++;
+
+  }
+  
+  
+if (n==0)
+{
+  return 1;
+}
+
+  return 0;
 }

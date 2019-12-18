bc7a0dbffee0473edef387d945d3bf2b8daa92f8
elonezra <elonezra1995@hotmail.com>
2019-12-18 23:34:34 +0200

    isort workk

--- isort.c
+++ isort.c
@@ -7,5 +7,41 @@ for ( j = (arr+i);  j> arr ;j--)
    {
        *j = *(j-1);
     }  
+}
 
+void insertion_sort(int* arr, int len)
+{
+
+  int i, key, j; 
+    for (i = 1; i < len; i++) { 
+        key = *(arr+i); 
+        j = i - 1; 
+        while (j >= 0 && *(arr+j) > key) { 
+            *(arr +j+1) = *(arr+j); 
+            j = j - 1; 
+        } 
+        *(arr+j + 1) = key; 
+    } 
+}
+
+int main() 
+{ 
+    int arr[] = { 12, 11, 13, 5, 6 }; 
+    int n = sizeof(arr) / sizeof(arr[0]); 
+   for (int i = 0; i < 5; i++)
+    {
+      
+      printf(" %d ",arr[i]);
+
+    }
+    printf("\n");
+    insertion_sort(arr, n); 
+    for (int i = 0; i < 5; i++)
+    {
+      
+      printf(" %d ",arr[i]);
+
+    }
+      
+    return 0; 
 }
